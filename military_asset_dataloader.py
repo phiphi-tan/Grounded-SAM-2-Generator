f6c1746d22aad66b7a5e50455bdb7be879f2a90a
Phi <phiphi.tan23@gmail.com>
2025-07-10 11:00:20 +0800

    Update

---
+++ military_asset_dataloader.py
@@ -0,0 +1,41 @@
+import kagglehub
+from datasets import load_dataset, Dataset
+
+
+def sample_images(split_size=3):
+    input_dataset = get_dataset(split_name='test')
+    input_dataset = sample_dataset(input_dataset, split_size)
+    return input_dataset['image'] 
+
+def get_dataset(split_name):
+    path = kagglehub.dataset_download("rawsi18/military-assets-dataset-12-classes-yolo8-format")
+
+    split = split_name # can be set to train or val as well
+    path_subset = path + "\\military_object_dataset\\" + split
+
+    image_dataset = load_dataset(path_subset + "\\images")['train']
+    label_dataset = load_dataset("text", data_dir=path_subset + "\\labels", sample_by="document")['train']
+
+    data = {
+        'image': image_dataset['image'],
+        'text': label_dataset['text'] 
+    }
+
+    ds = Dataset.from_dict(data)
+
+    return ds
+    
+
+def sample_dataset(ds, split_size):
+    return_ds = ds.shuffle(seed=split_size)
+    return_ds = return_ds.select(range(split_size))
+    return return_ds
+
+def get_images(split='test', start=0, end=None):
+    dataset = get_dataset(split)
+
+    if end == None:
+        return dataset
+    
+    return_ds = dataset.select(range(start, end))
+    return return_ds['image']
---
+++ visualise.py
@@ -0,0 +1,56 @@
+import cv2
+from PIL import Image
+import json
+import numpy as np
+import supervision as sv
+from supervision.draw.color import ColorPalette
+from utils.supervision_utils import CUSTOM_COLOR_MAP
+import os
+import pycocotools.mask as mask_util
+from pathlib import Path
+
+# input is a dictionary of format
+# {"segmentation": {"size": [H, W], "counts": "{RLE_mask_encoding}"}}
+def decode_segmentation(segmentation):
+    size = tuple(segmentation['size'])
+    counts = segmentation['counts']
+    mask = mask_util.decode({'size': size, 'counts': counts.encode('utf-8')})
+    return mask
+
+OUTPUT_DIR = Path('outputs/military-assets-segmentised/test/output')
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True) # create output directory
+
+DATA_DIR = 'outputs/grounded_sam2_hf_generator/test/'
+
+img = Image.open('outputs/military-assets-segmentised/test/000001.jpg')
+img_cv2 = cv2.imread('outputs/military-assets-segmentised/test/000001.jpg')
+
+data = json.load(open("outputs/military-assets-segmentised/test/000001_data.json"))
+data_annotations = data['annotations']
+
+input_boxes = np.array([annotation['bbox'] for annotation in data_annotations])
+class_labels = np.array(["{} {:.2f} (mask {:.2f})".format(annotation['class_name'], annotation['confidence'], annotation['score']) for annotation in data_annotations])
+class_ids = np.array(list(range(len(class_labels))))
+print(class_labels)
+
+masks = np.array([decode_segmentation(annotation['segmentation']) for annotation in data_annotations])
+scores = np.array([annotation['score'] for annotation in data_annotations])
+
+detections = sv.Detections(
+    xyxy=input_boxes,  # (n, 4)
+    mask=masks.astype(bool),  # (n, h, w)
+    class_id=class_ids
+)
+
+# create the
+box_annotator = sv.BoxAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+annotated_frame = box_annotator.annotate(scene=img_cv2.copy(), detections=detections)
+label_annotator = sv.LabelAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=class_labels)
+
+cv2.imwrite(os.path.join(OUTPUT_DIR, "bbox.jpg"), annotated_frame)
+
+mask_annotator = sv.MaskAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+annotated_frame = mask_annotator.annotate(scene=annotated_frame, detections=detections)
+
+cv2.imwrite(os.path.join(OUTPUT_DIR, "bbox_masked.jpg"), annotated_frame)
