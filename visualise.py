9055b45f6ade95ca61f49663df6bcf78c8b7dafc
Tan Phinehas Faithful (IA) <tfaithfu@dso.org.sg>
2025-07-10 17:20:27 +0800

    change visualisation script

--- visualise.py
+++ visualise.py
@@ -9,48 +9,54 @@ import os
 import pycocotools.mask as mask_util
 from pathlib import Path
 
-# input is a dictionary of format
-# {"segmentation": {"size": [H, W], "counts": "{RLE_mask_encoding}"}}
-def decode_segmentation(segmentation):
-    size = tuple(segmentation['size'])
-    counts = segmentation['counts']
-    mask = mask_util.decode({'size': size, 'counts': counts.encode('utf-8')})
-    return mask
-
+INPUT_DIR = Path('outputs/grounded_sam2_hf_generator/test/')
 OUTPUT_DIR = Path('outputs/military-assets-segmentised/test/output')
+
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True) # create output directory
 
-DATA_DIR = 'outputs/grounded_sam2_hf_generator/test/'
+for file in os.listdir(INPUT_DIR):
+    if not (file.endswith(".jpg")):
+        continue
 
-img = Image.open('outputs/military-assets-segmentised/test/000001.jpg')
-img_cv2 = cv2.imread('outputs/military-assets-segmentised/test/000001.jpg')
+    image = file
+    img_cv2 = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-data = json.load(open("outputs/military-assets-segmentised/test/000001_data.json"))
-data_annotations = data['annotations']
+    image_name = image.filename.split("\\")[-1].rstrip('.jpg')
+    data_file_name = image_name + '_data.json'
 
-input_boxes = np.array([annotation['bbox'] for annotation in data_annotations])
-class_labels = np.array(["{} {:.2f} (mask {:.2f})".format(annotation['class_name'], annotation['confidence'], annotation['score']) for annotation in data_annotations])
-class_ids = np.array(list(range(len(class_labels))))
-print(class_labels)
+    try:
+        data = json.load(open(os.path.join(INPUT_DIR, data_file_name)))
+    except FileNotFoundError:
+        print("No corresponding data file found for image {}.jpg",format(image_name))
+        continue
+    data_annotations = data['annotations']
 
-masks = np.array([decode_segmentation(annotation['segmentation']) for annotation in data_annotations])
-scores = np.array([annotation['score'] for annotation in data_annotations])
+    input_boxes = np.array([annotation['bbox'] for annotation in data_annotations])
+    class_labels = np.array(["{} {:.2f} (mask {:.2f})".format(annotation['class_name'], annotation['confidence'], annotation['score'][0]) for annotation in data_annotations])
+    class_ids = np.array(list(range(len(class_labels))))
 
-detections = sv.Detections(
-    xyxy=input_boxes,  # (n, 4)
-    mask=masks.astype(bool),  # (n, h, w)
-    class_id=class_ids
-)
+    # mask input is a dictionary of format
+    # {"segmentation": {"size": [H, W], "counts": "{RLE_mask_encoding}"}}
+    def decode_segmentation(segmentation):
+        size = tuple(segmentation['size'])
+        counts = segmentation['counts']
+        mask = mask_util.decode({'size': size, 'counts': counts.encode('utf-8')})
+        return mask
 
-# create the
-box_annotator = sv.BoxAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = box_annotator.annotate(scene=img_cv2.copy(), detections=detections)
-label_annotator = sv.LabelAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=class_labels)
+    masks = np.array([decode_segmentation(annotation['segmentation']) for annotation in data_annotations])
 
-cv2.imwrite(os.path.join(OUTPUT_DIR, "bbox.jpg"), annotated_frame)
+    detections = sv.Detections(
+        xyxy=input_boxes,  # (N, 4)
+        mask=masks.astype(bool),
+        class_id=class_ids
+    )
 
-mask_annotator = sv.MaskAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = mask_annotator.annotate(scene=annotated_frame, detections=detections)
+    # create the
+    box_annotator = sv.BoxAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+    annotated_frame = box_annotator.annotate(scene=img_cv2.copy(), detections=detections)
+    label_annotator = sv.LabelAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+    annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=class_labels)
+    mask_annotator = sv.MaskAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+    annotated_frame = mask_annotator.annotate(scene=annotated_frame, detections=detections)
 
-cv2.imwrite(os.path.join(OUTPUT_DIR, "bbox_masked.jpg"), annotated_frame)
+    cv2.imwrite(os.path.join(OUTPUT_DIR, "{}_constructed.jpg".format(image_name)), annotated_frame)
